3438691aaaf2440bb2c052ae5af1ded8f7f5b7c4
Beatriz <beaclemen90@gmail.com>
2020-06-09 16:08:02 +0200

    Commint incial

---
+++ app.js
@@ -0,0 +1,36 @@
+const argv = require('./config/yargs').argv;
+const colors = require('colors/safe');
+
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
+
+let comando = argv._[0];
+
+switch (comando) {
+    case 'listar':
+        //console.log('listar');
+        listarTabla(argv.base, argv.limite);
+        break;
+
+    case 'crear':
+        //console.log('crear');
+        crearArchivo(argv.base, argv.limite)
+            //.then(archivo => console.log(`Archivo creado: ${archivo.green}`))
+            .then(archivo => console.log(`Archivo creado: `, colors.green(archivo)))
+            .catch(e => console.log(e));
+        break;
+
+    default:
+        console.log('Comando no reconocido');
+}
+
+//console.log(process.argv);
+
+
+/*
+let parametro = argv[2];
+let base = parametro.split('=')[1];
+
+crearArchivo(base)
+    .then(archivo => console.log(`Archivo creado: ${archivo}`))
+    .catch(e => console.log(e));
+*/
